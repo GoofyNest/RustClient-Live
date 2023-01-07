9ec274107c13b2a35eedab8484ef00f7ceb1e236
BuildTools <unconfigured@null.spigotmc.org>
2023-01-07 12:28:50 +0100

    10271880 - 2023-01-07 12:28:48

--- BaseMountable.cs
+++ BaseMountable.cs
@@ -20,6 +20,7 @@ public class BaseMountable : BaseCombatEntity // TypeDefIndex: 10051
 	public bool disableMeshCullingForPlayers;
 	public bool allowHeadLook;
 	public bool ignoreVehicleParent;
+	public bool legacyDismount;
 	[FormerlySerializedAsAttribute]
 	public bool modifiesPlayerCollider;
 	public BasePlayer.CapsuleColliderInfo customPlayerCollider;
@@ -70,6 +71,8 @@ public class BaseMountable : BaseCombatEntity // TypeDefIndex: 10051
 	public virtual Vector2 GetYawClamp() { }
 
 	public virtual bool AnyMounted() { }
+
+	public bool IsMounted() { }
 
 	public virtual Vector3 EyePositionForPlayer(BasePlayer player, Quaternion lookRot) { }
 
@@ -168,6 +171,9 @@ public class BaseMountable : BaseCombatEntity // TypeDefIndex: 10051
 	public void .ctor() { }
 
 	private static void .cctor() { }
+
+	[CompilerGeneratedAttribute]
+	private bool <ValidDismountPosition>g__HitOurself|70_0(RaycastHit hit) { }
 
 	[CompilerGeneratedAttribute]
 	private void <DoPlayerMovement>
